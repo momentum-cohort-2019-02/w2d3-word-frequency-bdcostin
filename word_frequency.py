1cd6502476928eaad0a79b2da984e95f9c2272aa
bdcostin <bailey.costin@gmail.com>
2019-02-21 07:46:05 -0500

    Put list into a dictionary

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,10 +7,45 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def normalize_text(text):
+    """
+    Given a text, lowercases it, removes all punctuation, 
+    and replaces all whitespace with normal spaces. Multiple whitespace will
+    be compressed into a single space.
+    """
+    text = text.casefold()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
 
+    # Remove all punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
+
+def print_word_freq(filename):
+    """Read in `filename` and print out the frequency of words in that file."""
+    with open(filename) as file:
+        text = file.read()
+
+    text = normalize_text(text)
+    words = []
+    for word in text.split(" "):
+        if word != '' and word not in STOP_WORDS:
+            words.append(word)
+    counter = {}
+    for word in words:
+        if word in counter:
+            counter[word] += 1
+        else:
+           counter[word] = 1
+    print(counter)
+
+    ## What now?
+    # Get a dictionary of word frequencies and print it out
 
 if __name__ == "__main__":
     import argparse
@@ -17,7 +54,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Get the word frequency in a text file.')
     parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
+    args = parser.parse_args()    
 
     file = Path(args.file)
     if file.is_file():
